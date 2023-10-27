4cea88a42068e924feb819178b62e6b051d7bbc3
Rita Tang <doggymint@gmail.com>
2023-10-27 17:20:40 -0400

    Add router for /categories

--- server/app.js
+++ server/app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const categoriesRouter = require("./routes/categories");
 
 var app = express();
 
@@ -16,5 +17,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/categories', categoriesRouter);
 
 module.exports = app;
