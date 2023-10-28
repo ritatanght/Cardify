bf3bb5bf72810e670eb6f946e694c802ac40897c
Justin Hao <justinhao@live.com>
2023-10-28 14:10:41 -0400

    Add returning set id for card post requests

--- server/db/queries/sets.js
+++ server/db/queries/sets.js
@@ -2,11 +2,11 @@ const db = require("../../configs/db.config");
 
 const postSetData = (setData) => {
   const query = `
-  INSERT INTO sets (title, description, private, category_id, user_id)
-  VALUES ($1, $2, $3, $4, $5);
+  INSERT INTO sets (title, description, private, category_id)
+  VALUES ($1, $2, $3, $4) RETURNING id;
   `;
-  
-  return db.query(query, [setData.title, setData.description, setData.private, setData.category_id, setData.user_id])
+
+  return db.query(query, [setData.title, setData.description, setData.private, setData.category_id])
 }
 
 module.exports = { postSetData };
