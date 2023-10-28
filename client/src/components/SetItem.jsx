a4e44649d58917590df142f7bf74135417b934ff
Justin Hao <justinhao@live.com>
2023-10-28 19:36:42 -0400

    Adjust styling

--- client/src/components/SetItem.jsx
+++ client/src/components/SetItem.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHeart } from '@fortawesome/free-solid-svg-icons'
+import '../assets/styles/setItem.scss'
 
 
 const SetItem = (props) => {
@@ -8,10 +9,10 @@ const SetItem = (props) => {
 
   return (
     <div className="set-item-container">
-      <h1>{'Set Name'}</h1>
+      <h2>{'set_name'}</h2>
       <div className="set-item-right">
-        <FontAwesomeIcon icon={faHeart} onClick={handleFavoriteClick} />
-        <h2>{'Set Owner'}</h2>
+        <FontAwesomeIcon className='fav-icon' icon={faHeart} onClick={handleFavoriteClick} />
+        <h2>{'username'}</h2>
       </div>
     </div>
   )
