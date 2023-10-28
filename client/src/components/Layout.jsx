8582d070e1979f04aebf2cb09e919755c7bafc8e
Justin Hao <justinhao@live.com>
2023-10-28 16:47:52 -0400

    Test code for viewing component

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
+import SetItem from "./SetItem"
 
 const Layout = () => {
   return (
     <>
       <Header />
       <Outlet />
+      <SetItem />
     </>
   );
 };
