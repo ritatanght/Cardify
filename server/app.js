88bc88a4049015504b8efcac866345a68e7adcc3
Justin Hao <justinhao@live.com>
2023-10-28 11:36:38 -0400

    Create route and query

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var setsRouter = require('./routes/sets')
+const setsRouter = require('./routes/sets')
 
 var app = express();
 
@@ -17,6 +17,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/sets', setsRouter);
+app.use('/api/sets', setsRouter);
 
 module.exports = app;
--- server/routes/sets.js
+++ server/routes/sets.js
@@ -1,7 +1,15 @@
 const router = require('express').Router();
+const sets = require("../db/queries/sets")
 
 router.post('/create', (req, res) => {
-  
+  sets.postSetData(req.body)
+    .then(result => {
+      res.status(201).json({ success: true, message: "Set created successfully", data: result });
+    })
+    .catch(err => {
+      res.status(500)
+      console.err(err)
+    })
 });
 
 module.exports = router;
