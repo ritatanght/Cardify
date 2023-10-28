743d88e9751db67a74748e9fc9c2524903b018c4
ritatanght <doggymint@gmail.com>
2023-10-28 18:58:17 -0400

    Update to handle nonexistent set and error

--- server/routes/sets.js
+++ server/routes/sets.js
@@ -1,5 +1,5 @@
-const router = require('express').Router();
-const sets = require("../db/queries/sets")
+const router = require("express").Router();
+const sets = require("../db/queries/sets");
 const cards = require("../db/queries/cards");
 
 router.post('/create', (req, res) => {
@@ -13,15 +13,17 @@ router.post('/create', (req, res) => {
     })
 });
 
-
 router.get("/:id", (req, res) => {
   const { id } = req.params;
   const setPromise = sets.getSetInfoById(id);
   const cardsPromise = cards.getCardsBySetId(id);
 
-  Promise.all([setPromise, cardsPromise]).then(([set, cards]) => {
-    res.json({ set, cards });
-  });
+  Promise.all([setPromise, cardsPromise])
+    .then(([set, cards]) => {
+      if (!set) return res.status(404).json({ message: "Set not found" });
+      return res.json({ set, cards });
+    })
+    .catch((err) => console.error(err));
 });
 
 module.exports = router;
