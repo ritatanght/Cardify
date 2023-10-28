1e56b24975a67538b92b956710aede271ae9c39b
ritatanght <doggymint@gmail.com>
2023-10-28 11:39:55 -0400

    Add path for viewing set

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,8 +4,12 @@ import "./App.css";
 import Home from "./routes/Home";
 import Category from "./routes/Category";
 import Sets from "./components/Sets";
+import ViewSet from "./routes/ViewSet";
+
 
 function App() {
+
+
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
@@ -14,6 +18,7 @@ function App() {
         <Route index element={<Home />} />
         <Route path="/category" element={<Category />} />
         <Route path="/sets/create" element={<Sets />} />
+        <Route path="/sets/:setId" element={<ViewSet />} />
       </Route>
     </Routes>
   );
