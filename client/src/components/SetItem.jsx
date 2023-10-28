5b915a164a8f9e553d548ccd1f2f04e4151d941d
Justin Hao <justinhao@live.com>
2023-10-28 16:44:17 -0400

    Initial SetItem commit

---
+++ client/src/components/SetItem.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHeart } from '@fortawesome/free-solid-svg-icons'
+
+
+const SetItem = (props) => {
+  const { name, owner, handleFavoriteClick } = props
+
+  return (
+    <div className="set-item-container">
+      <h1>{'Set Name'}</h1>
+      <FontAwesomeIcon icon={faHeart} onClick={handleFavoriteClick}/>
+      <h2>{'Set Owner'}</h2>
+    </div>
+  )
+}
+
+export default SetItem;
