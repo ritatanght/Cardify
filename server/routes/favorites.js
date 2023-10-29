3c5c0925cdc60ebaa5b8831ad4a8a3e02785d8ee
ritatanght <doggymint@gmail.com>
2023-10-28 21:23:44 -0400

    Add endpoints for /api/favorites

---
+++ server/routes/favorites.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const router = express.Router();
+const favorites = require("../db/queries/favorites");
+
+router.get("/:userId", (req, res) => {
+  const { userId } = req.params;
+  favorites.getFavoritesByUserId(userId).then((data) => {
+    const favoritedSets = data.map((item) => item.set_id);
+    // return an array of all the favorited set_id
+    return res.json(favoritedSets);
+  });
+});
+
+router.post("/", (req, res) => {
+  const { userId, setId } = req.body;
+
+  favorites
+    .addFavoriteByUserAndSet(userId, setId)
+    .then(() => res.status(201).end());
+});
+
+router.put("/", (req, res) => {
+  const { userId, setId } = req.body;
+
+  favorites
+    .removeFavoriteByUserAndSet(userId, setId)
+    .then(() => res.status(200).end());
+});
+
+module.exports = router;
