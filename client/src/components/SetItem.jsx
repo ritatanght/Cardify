3890af723890089cb63640745a3b2e82c5231840
Justin Hao <justinhao@live.com>
2023-10-30 15:47:44 -0400

    Add favorite button

--- client/src/components/SetItem.jsx
+++ client/src/components/SetItem.jsx
@@ -1,21 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
+import useFavButton from '../hooks/useFavButton';
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHeart } from '@fortawesome/free-solid-svg-icons'
+import { faHeart as fillHeart } from "@fortawesome/free-solid-svg-icons";
+import { faHeart as emptyHeart } from "@fortawesome/free-regular-svg-icons";
+import Button from "react-bootstrap/esm/Button";
 import '../assets/styles/setItem.scss'
 
-
 const SetItem = (props) => {
-  const { set, user, handleFavoriteClick } = props
+  const { set, user, initiallyLiked } = props;
+  const { isLiked, setIsLiked, toggleLike } = useFavButton();
 
+  useEffect(() => {
+    setIsLiked(initiallyLiked)
+  }, [setIsLiked])
+
+  const handleLikeClick = () => {
+    toggleLike(user.id, set.id);
+  }
+  
   return (
     <div className="set-item-container">
       <Link to={`/sets/${set.id}`}>
         <h2>{set.title}</h2>
       </Link>
       <div className="set-item-right">
-        <FontAwesomeIcon className='fav-icon' icon={faHeart} onClick={() => handleFavoriteClick()} />
-        <h2>{user}</h2>
+        <Button variant="link" onClick={() => handleLikeClick()}>
+          {isLiked ? (
+            <FontAwesomeIcon icon={fillHeart} />
+          ) : (
+            <FontAwesomeIcon icon={emptyHeart} />
+          )}
+        </Button>
+        <h2>{user.username}</h2>
       </div>
     </div>
   )
--- client/src/routes/Profile.jsx
+++ client/src/routes/Profile.jsx
@@ -35,12 +35,12 @@ const Profile = () => {
       <Tabs defaultActiveKey='my-sets'>
         <Tab eventKey="my-sets" title="My Sets">
           {sets.map(set => (
-            <SetItem key={set.id} set={set} user={name.username}/>
+            <SetItem key={set.id} set={set} user={name} initiallyLiked={favoriteSets.some(favorite => favorite.id === set.id)}/>
           ))}
         </Tab>
         <Tab eventKey="favorite-sets" title="Favorite Sets">
           {favoriteSets.map(favorite => (
-            <SetItem key={favorite.id} set={favorite} user={name.username} />
+            <SetItem key={favorite.id} set={favorite} user={name} initiallyLiked={true}/>
           ))}
         </Tab>
       </Tabs>
