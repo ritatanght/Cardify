e1968f3a9371ef4b150064120dbf14f4301faa90
Rita Tang <doggymint@gmail.com>
2023-10-29 21:22:51 -0400

    Add basic structure and state variables

--- client/src/components/Cards.jsx
+++ client/src/components/Cards.jsx
@@ -1,6 +1,34 @@
+import { useState } from "react";
+import CardItem from "./CardItem";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import Button from "react-bootstrap/Button";
+
 const Cards = (props) => {
-  console.log(props.cards)
-  return <div>Cards</div>;
+  const [currCard, setCurrCard] = useState(1);
+  const [isFinished, setIsFinished] = useState(false);
+  const { front, back } = props.cards[currCard - 1];
+
+
+  return (
+    <div className="cards-container">
+      <CardItem/>
+      {!isFinished && (
+        <div className="cards-navigation">
+          <Button variant="link">
+            <FontAwesomeIcon icon={faArrowLeft} />
+          </Button>
+          <span>
+            {currCard}/{props.cards.length}
+          </span>
+          <Button variant="link">
+            <FontAwesomeIcon icon={faArrowRight} />
+          </Button>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Cards;
