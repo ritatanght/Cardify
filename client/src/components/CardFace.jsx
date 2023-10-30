2c2cf4ca46361a08a2ea3fafb7acd20649b53089
Rita Tang <doggymint@gmail.com>
2023-10-30 14:47:02 -0400

    Add text-to-speech to card

--- client/src/components/CardFace.jsx
+++ client/src/components/CardFace.jsx
@@ -3,10 +3,20 @@ import { faVolumeHigh } from "@fortawesome/free-solid-svg-icons";
 import { Button } from "react-bootstrap";
 
 const CardFace = ({ position, text }) => {
+  
   const speakText = (e) => {
     e.stopPropagation();
-    console.log("play speech");
+    const synth = window.speechSynthesis;
+    if (!synth)
+      return console.error("Your browser does not support Speech Synthesis");
+
+    const utterance = new SpeechSynthesisUtterance(text);
+    // Change voice for the speech; 7 - Google US English
+    utterance.voice = synth.getVoices()[7];
+
+    synth.speak(utterance);
   };
+
   return (
     <div className={`card-${position}`}>
       <div className="card__icons-container">
