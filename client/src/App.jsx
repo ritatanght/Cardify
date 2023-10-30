c3e398125ddeea33cb3b06fd291930f44cc6eb4e
Justin Hao <justinhao@live.com>
2023-10-30 16:49:14 -0400

    Initial search frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ import Home from "./routes/Home";
 import Category from "./routes/Category";
 import Sets from "./components/Sets";
 import ViewSet from "./routes/ViewSet";
-
+import Search from "./routes/Search";
 
 function App() {
 
@@ -18,6 +18,7 @@ function App() {
         <Route path="/category" element={<Category />} />
         <Route path="/sets/create" element={<Sets />} />
         <Route path="/sets/:setId" element={<ViewSet />} />
+        <Route path="/search/:query" element={<Search />} />
       </Route>
     </Routes>
   );
