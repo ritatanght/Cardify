d81ca1208088c21088400e84514ebd9745d47901
Rita Tang <doggymint@gmail.com>
2023-10-29 21:24:13 -0400

    Add onClick handler functions

--- client/src/components/Cards.jsx
+++ client/src/components/Cards.jsx
@@ -10,20 +10,39 @@ const Cards = (props) => {
   const [isFinished, setIsFinished] = useState(false);
   const { front, back } = props.cards[currCard - 1];
 
+  const resetCard = () => {
+    setCurrCard(1);
+    setIsFinished(false);
+  };
+
+  const prevCard = () => {
+    setCurrCard((prevIndex) => (prevIndex === 1 ? 1 : prevIndex - 1));
+  };
+
+  const nextCard = () => {
+    setCurrCard((prevIndex) => {
+      if (prevIndex + 1 <= props.cards.length) {
+        return prevIndex + 1;
+      } else {
+        setIsFinished(true);
+        return prevIndex;
+      }
+    });
+  };
 
   return (
     <div className="cards-container">
       <CardItem/>
       {!isFinished && (
         <div className="cards-navigation">
-          <Button variant="link">
+          <Button variant="link" onClick={prevCard}>
             <FontAwesomeIcon icon={faArrowLeft} />
           </Button>
           <span>
             {currCard}/{props.cards.length}
           </span>
           <Button variant="link">
-            <FontAwesomeIcon icon={faArrowRight} />
+            <FontAwesomeIcon icon={faArrowRight} onClick={nextCard} />
           </Button>
         </div>
       )}
