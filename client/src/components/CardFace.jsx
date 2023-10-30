ea13059f0cea1e7e087225f158a268694f442471
Rita Tang <doggymint@gmail.com>
2023-10-30 13:29:15 -0400

    Add CardFace component to render a side of a card

---
+++ client/src/components/CardFace.jsx
@@ -0,0 +1,23 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faVolumeHigh } from "@fortawesome/free-solid-svg-icons";
+import { Button } from "react-bootstrap";
+
+const CardFace = ({ position, text }) => {
+  const speakText = (e) => {
+    e.stopPropagation();
+    console.log("play speech");
+  };
+  return (
+    <div className={`card-${position}`}>
+      <div className="card__icons-container">
+        <Button variant="link" onClick={speakText}>
+          <FontAwesomeIcon icon={faVolumeHigh} />
+        </Button>
+      </div>
+      {position}
+      <p className="card__text">{text}</p>
+    </div>
+  );
+};
+
+export default CardFace;
