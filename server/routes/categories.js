e444f2d8864caa9c316ad3dad549864fbbf62ff9
ritatanght <doggymint@gmail.com>
2023-10-30 17:57:36 -0400

    Add endpoint for a specific category

--- server/routes/categories.js
+++ server/routes/categories.js
@@ -1,8 +1,22 @@
 const router = require("express").Router();
 const categories = require("../db/queries/categories");
+const sets = require("../db/queries/sets");
 
 router.get("/", (req, res) => {
   categories.getAllCategories().then((data) => res.json(data));
+});
+
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  const categoryPromise = categories.getCategoryById(id);
+  const setsPromise = sets.getSetsByCategoryId(id);
+
+  Promise.all([categoryPromise, setsPromise]).then(
+    ([category, sets]) => {
+      res.json({ category: category.name, sets });
+    }
+
+  );
 });
 
 module.exports = router;
