0dd64af1eb4dbe4f85d19b7dc4aedee8f6900dbe
Justin Hao <justinhao@live.com>
2023-10-30 15:55:55 -0400

    Revert "Set up API for username"
    This reverts commit 9e9e8180ba0d9f86f278893fcd04811ab24d2181.

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,16 +1,9 @@
 const router = require('express').Router();
-const users = require('../db/queries/users')
 
-router.get('/:id', (req, res) => {
-  const { id } = req.params
+const users = ['Bob', 'Alex', 'Will', 'Tristan'];
 
-  users.getUserUsername(id)
-    .then(data => { data ? res.json(data) : res.status(404).json({ message: "User not found" }) })
-    .catch(err => {
-      console.error(err)
-      res.status(500).json({ message: "Something went wrong" })
-    })
-
+router.get('/', (req, res) => {
+  res.json(users);
 });
 
 module.exports = router;
