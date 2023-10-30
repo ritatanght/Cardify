a6b2a7cb8043a9efee6894ec3142f68427a2f0b9
ritatanght <doggymint@gmail.com>
2023-10-29 21:35:02 -0400

    Add handler function for TextToSpeech button

--- client/src/components/CardItem.jsx
+++ client/src/components/CardItem.jsx
@@ -6,9 +6,14 @@ import { Button } from "react-bootstrap";
 const CardItem = ({ front, back }) => {
   const [isFront, setIsFront] = useState(true);
 
+  const speakText = (e) => {
+    e.stopPropagation();
+    console.log("play speech");
+  };
+
   return (
     <div className="Card" onClick={() => setIsFront(!isFront)}>
-      <Button variant="link">
+      <Button variant="link" onClick={speakText}>
         <FontAwesomeIcon icon={faVolumeHigh} />
       </Button>
       {isFront ? front : back}
