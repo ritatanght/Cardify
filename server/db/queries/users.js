56745d8922f22c1ca7ac3755e150011d03b0fa51
Rita Tang <doggymint@gmail.com>
2023-10-31 11:59:52 -0400

    Update query to also include user id

--- server/db/queries/users.js
+++ server/db/queries/users.js
@@ -1,15 +1,15 @@
-const db = require("../../configs/db.config")
+const db = require("../../configs/db.config");
 
 const getUserUsername = (userId) => {
   const query = `
-   SELECT users.username
+   SELECT id, username
    FROM users
-   WHERE users.id = $1;
+   WHERE users.id = $1
+   AND deleted = false;
   `;
-  return db.query(query, [userId])
-    .then(data => data.rows[0])
+  return db.query(query, [userId]).then((data) => data.rows[0]);
 };
 
 module.exports = {
-  getUserUsername
-}
+  getUserUsername,
+};
