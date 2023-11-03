411673e5f98edd60799e5e821d127b6a9474cfad
ritatanght <doggymint@gmail.com>
2023-11-03 16:05:56 -0400

    Take out image2 for consistent size

--- client/src/components/BackgroundChanger.jsx
+++ client/src/components/BackgroundChanger.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import image1 from '../assets/images/Background_img1.png';
-import image2 from '../assets/images/Background_img2.png';
+//import image2 from '../assets/images/Background_img2.png';
 import image3 from '../assets/images/Background_img3.png';
 import image4 from '../assets/images/Background_img4.png';
 
 
-const images = [image1, image2, image3, image4 ];
+const images = [image1, image3, image4 ];
 
 const BackgroundChanger = ({ containerRef }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -14,7 +14,6 @@ const BackgroundChanger = ({ containerRef }) => {
       const changeBackground = () => {
           if (containerRef.current) { // Ensure the ref is available
               containerRef.current.style.backgroundImage = `url(${images[currentIndex]})`;
-              
           }
           setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
       };
