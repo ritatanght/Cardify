68e26846e38eefc4d111fe3032bcb06da33c9dfb
ritatanght <doggymint@gmail.com>
2023-11-03 16:08:07 -0400

    Add banner container and populate site info

--- client/src/routes/Home.jsx
+++ client/src/routes/Home.jsx
@@ -1,37 +1,53 @@
-
-import React, { useRef } from 'react';
-import { Button } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-import '../assets/styles/home.scss';  
-import '../assets/styles/BackgroundStyles.scss'
-import BackgroundChanger from '../components/BackgroundChanger';
+import React, { useRef } from "react";
+import { Button } from "react-bootstrap";
+import "../assets/styles/home.scss";
+import "../assets/styles/BackgroundStyles.scss";
+import BackgroundChanger from "../components/BackgroundChanger";
 
 const Home = () => {
-    const homeContainerRef = useRef(null); // Create a ref
-    return (
-        <div className="home-container" ref={homeContainerRef}>
-            <BackgroundChanger containerRef={homeContainerRef} />
-        
-            <Link to="/sets/create">
-                <Button variant="primary" as="span">Create</Button>
-            </Link>
-            <div className="columns-container">
-                <div className="column">
-                    <h3>Info about creating sets</h3>
-                    <p>Details and steps to create your own flashcard sets.</p>
-                </div>
-                <div className="column">
-                    <h3>Info on TTS reading flashcards to you</h3>
-                    <p>Discover the benefits of text-to-speech technology for learning.</p>
-                </div>
-                <div className="column">
-                    <h3>Info on checking out other sets</h3>
-                    <p>Explore and study from a variety of flashcard sets created by others.</p>
-                </div>
-            </div>
+  const bannerContainerRef = useRef(null); // Create a ref
+  return (
+    <div className="home-container">
+      <div className="banner-container" ref={bannerContainerRef}>
+        <BackgroundChanger containerRef={bannerContainerRef} />
+        <div className="banner__content">
+          <h2>
+            Get Started with <br />
+            Your Flashcard Set
+          </h2>
+          <Button variant="primary" href="/sets/create">
+            Create My Set
+          </Button>
         </div>
-        
-    );
+      </div>
+      <div className="columns-container">
+        <div className="column">
+          <h3>Create Your Own Flashcards Sets</h3>
+          <p>
+            Create personalized flashcard sets for your unique learning needs.
+            Whether it&apos;s for language learning, test preparation, or any
+            other subject, start creating your sets in just a few simple steps.
+          </p>
+        </div>
+        <div className="column">
+          <h3>Elevate Your Learning Journey</h3>
+          <p>
+            Discover the power of text-to-speech technology for a more engaging
+            and immersive learning experience. Let your flashcards come to life
+            with audio assistance.
+          </p>
+        </div>
+        <div className="column">
+          <h3>Explore and Learn from Others</h3>
+          <p>
+            Dive into a world of knowledge with flashcard sets crafted by our
+            community of avid learners. Browse and study from a wide range of
+            topics.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
