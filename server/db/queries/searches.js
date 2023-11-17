a340aeba2fcafe3c4424e7b7820515f050004755
ritatanght <doggymint@gmail.com>
2023-11-16 21:32:59 -0500

    Update query to exclude deleted set for search

--- server/db/queries/searches.js
+++ server/db/queries/searches.js
@@ -1,19 +1,20 @@
 const db = require("../../configs/db.config");
 
 const searchByText = (searchQuery) => {
-  const searchTerm = `%${searchQuery}%`
+  const searchTerm = `%${searchQuery}%`;
 
   const query = `
-    SELECT sets.title, sets.id , sets.deleted, users.username AS username, categories.name AS category
+    SELECT sets.title, sets.id, users.username AS username, categories.name AS category
     FROM sets 
     JOIN users ON sets.user_id = users.id
     JOIN categories ON sets.category_id = categories.id 
-    WHERE sets.title ILIKE $1 OR sets.description ILIKE $1 OR categories.name ILIKE $1;
+    WHERE (sets.title ILIKE $1 OR sets.description ILIKE $1 OR categories.name ILIKE $1)
+    AND sets.deleted = false
   `;
 
-  return db.query(query, [searchTerm])
-}
+  return db.query(query, [searchTerm]);
+};
 
 module.exports = {
-  searchByText
-}
+  searchByText,
+};
