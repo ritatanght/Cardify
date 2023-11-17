e9d1b6551993b88284a4d3a235c822712a277b78
ritatanght <doggymint@gmail.com>
2023-11-17 16:21:22 -0500

    Add state and event handler to register form

--- client/src/routes/Register.jsx
+++ client/src/routes/Register.jsx
@@ -1,42 +1,66 @@
+import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import FloatingLabel from "react-bootstrap/FloatingLabel";
 import Form from "react-bootstrap/Form";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const Register = () => {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post("/api/users", { email, username, password })
+      .then((res) => {
+        if (res.status === 200) {
+          //res.data is the user data
+          navigate(`/users/${res.data.id}`);
+        }
+      })
+      .catch((err) => console.error(err.response.data.message));
+  };
   return (
     <main>
       <h1>Register</h1>
-      <p>Already have an account? <Link to="/login">Login</Link></p>
-      <Form>
+      <p>
+        Already have an account? <Link to="/login">Login</Link>
+      </p>
+      <Form onSubmit={handleSubmit}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
-          <FloatingLabel
-            controlId="floatingInput"
-            label="Email address"
-            className="mb-3"
-          >
-            <Form.Control type="email" placeholder="Enter email"/>
+          <FloatingLabel label="Email address" className="mb-3">
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
           </FloatingLabel>
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicUsername">
-          <FloatingLabel
-            controlId="floatingInput"
-            label="Username"
-            className="mb-3"
-          >
-            <Form.Control type="text" placeholder="Enter username" />
+          <FloatingLabel label="Username" className="mb-3">
+            <Form.Control
+              type="text"
+              placeholder="Enter username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
           </FloatingLabel>
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
-          <FloatingLabel
-            controlId="floatingInput"
-            label="Password"
-            className="mb-3"
-          >
-            <Form.Control type="password" placeholder="Password" />
+          <FloatingLabel label="Password" className="mb-3">
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
           </FloatingLabel>
         </Form.Group>
         <Button variant="primary" type="submit">
