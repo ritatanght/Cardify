7df70dc2a7f1c96c50dd091aa8c811c3f88a9532
ritatanght <doggymint@gmail.com>
2023-11-17 21:07:31 -0500

    Add login form and event handler

--- client/src/routes/Login.jsx
+++ client/src/routes/Login.jsx
@@ -1,7 +1,51 @@
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import FloatingLabel from "react-bootstrap/FloatingLabel";
+import Form from "react-bootstrap/Form";
+import { Link } from "react-router-dom";
+
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    console.log("login");
+  };
+
   return (
     <main>
       <h1>Login</h1>
+
+      <Form onSubmit={handleLogin}>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <FloatingLabel label="Email address" className="mb-3">
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </FloatingLabel>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <FloatingLabel label="Password" className="mb-3">
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </FloatingLabel>
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Login
+        </Button>
+      </Form>
+      <p className="mt-4">
+        Don&apos;t have an account? <Link to="/register">Register</Link>
+      </p>
     </main>
   );
 };
