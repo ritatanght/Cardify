e791f82662aa87cdf6007b86f19724f84f431b6b
ritatanght <doggymint@gmail.com>
2023-11-21 17:13:36 -0500

    Redirect to login when user is visitor

--- client/src/routes/CreateSet.jsx
+++ client/src/routes/CreateSet.jsx
@@ -1,115 +1,138 @@
-import React, { useState, useEffect } from 'react'
-import { useUser } from '../context/UserProvider'
-import Button from 'react-bootstrap/Button'
-import Form from 'react-bootstrap/Form'
-import FloatingLabel from 'react-bootstrap/esm/FloatingLabel'
-import Dropdown from 'react-bootstrap/Dropdown'
-import { useNavigate } from 'react-router-dom'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTrash } from '@fortawesome/free-solid-svg-icons'
-import axios from 'axios'
-import "../assets/styles/EditSet.scss"
+import React, { useState, useEffect } from "react";
+import { useUser } from "../context/UserProvider";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import FloatingLabel from "react-bootstrap/esm/FloatingLabel";
+import Dropdown from "react-bootstrap/Dropdown";
+import { useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import { Navigate } from "react-router-dom";
+import axios from "axios";
+import "../assets/styles/EditSet.scss";
 
 const CreateSet = () => {
-  const navigate = useNavigate()
-  const { user } = useUser()
+  const navigate = useNavigate();
+  const { user, clearUserInfo } = useUser();
 
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [selectedCategory, setSelectedCategory] = useState({});
-  const [categories, setCategories] = useState([])
+  const [categories, setCategories] = useState([]);
   const [isPrivate, setIsPrivate] = useState(false);
   const [cards, setCards] = useState([
-    { front: '', back: '' },
-    { front: '', back: '' },
-    { front: '', back: '' }
+    { front: "", back: "" },
+    { front: "", back: "" },
+    { front: "", back: "" },
   ]);
 
+  useEffect(() => {
+    axios
+      .get("/api/categories/")
+      .then((response) => {
+        setCategories(response.data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
+
+  // If user is not logged-in, redirect to login page
+  if (!user) return <Navigate to="/login" replace={true} />;
 
   const setformData = {
     title,
     description,
     category_id: selectedCategory.id,
     private: isPrivate,
-    user_id: user.id
-  }
+    user_id: user.id,
+  };
 
   const cardFormData = cards;
 
-  useEffect(() => {
-    axios.get('/api/categories/')
-      .then(response => {
-        setCategories(response.data)
-      })
-      .catch(err => {
-        console.error(err)
-      })
-  }, [])
-
   const handleSubmit = (event) => {
     event.preventDefault();
-    axios.post("/api/sets/create", setformData)
-      .then(result => {
-        const setId = result.data.data.rows[0].id
-        const cardDataWithSetId = cardFormData.map(card => ({ ...card, setId }));
+    axios
+      .post("/api/sets/create", setformData)
+      .then((result) => {
+        const setId = result.data.data.rows[0].id;
+        const cardDataWithSetId = cardFormData.map((card) => ({
+          ...card,
+          setId,
+        }));
         axios.post("/api/cards/create", cardDataWithSetId); // TODO: Adjust this endpoint
       })
-      .then(() => { navigate(`/users/${user.id}`) })
-      .catch(err => { console.error(err) })
-  }
+      .then(() => {
+        navigate("/profile");
+      })
+      .catch((err) => {
+         if (err.response.status === 401) {
+           console.log(err.response.data.message);
+           clearUserInfo();
+         } else {
+           console.error(err);
+         }
+      });
+  };
 
   const addCard = () => {
-    const newCards = [...cards, {
-      front: "",
-      back: "",
-      deleted: false
-    }]
-    setCards(newCards)
-  }
+    const newCards = [
+      ...cards,
+      {
+        front: "",
+        back: "",
+        deleted: false,
+      },
+    ];
+    setCards(newCards);
+  };
 
   const handleDelete = (cardIndex) => {
     const updatedCards = [...cards];
     updatedCards.splice(cardIndex, 1);
     setCards(updatedCards);
-  }
+  };
 
   return (
     <div className="create-container">
       <Form>
-        <div className='set-container'>
-          <div className='set-header-container'>
+        <div className="set-container">
+          <div className="set-header-container">
             <h1>Create a New Set</h1>
-            <Button variant='primary' type='submit' onClick={handleSubmit}>Create</Button>
+            <Button variant="primary" type="submit" onClick={handleSubmit}>
+              Create
+            </Button>
           </div>
-          <div className='set-info-container'>
-            <FloatingLabel label='Title'>
+          <div className="set-info-container">
+            <FloatingLabel label="Title">
               <Form.Control
-                type='text'
-                placeholder='Title'
+                type="text"
+                placeholder="Title"
                 value={title}
-                onChange={e => setTitle(e.target.value)}
+                onChange={(e) => setTitle(e.target.value)}
               />
             </FloatingLabel>
-            <div className='set-info-details'>
-              <FloatingLabel label='Description'>
+            <div className="set-info-details">
+              <FloatingLabel label="Description">
                 <Form.Control
-                  as='textarea'
-                  placeholder='Description'
+                  as="textarea"
+                  placeholder="Description"
                   value={description}
-                  onChange={e => setDescription(e.target.value)}
-                  style={{ height: '100px' }}
+                  onChange={(e) => setDescription(e.target.value)}
+                  style={{ height: "100px" }}
                 />
               </FloatingLabel>
-              <div className='set-info-options'>
+              <div className="set-info-options">
                 <Dropdown>
-                  <Dropdown.Toggle variant='success' id='dropdown-basic'>
+                  <Dropdown.Toggle variant="success" id="dropdown-basic">
                     {selectedCategory.name || "Select a category"}
                   </Dropdown.Toggle>
                   <Dropdown.Menu>
-                    {categories.map(category => (
+                    {categories.map((category) => (
                       <Dropdown.Item
                         key={category.id}
-                        onClick={() => setSelectedCategory(category)}>
+                        onClick={() => setSelectedCategory(category)}
+                      >
                         {category.name}
                       </Dropdown.Item>
                     ))}
@@ -120,7 +143,7 @@ const CreateSet = () => {
                   checked={isPrivate}
                   onChange={() => setIsPrivate(!isPrivate)}
                   reverse
-                  label='Private?'
+                  label="Private?"
                 />
               </div>
             </div>
@@ -128,42 +151,45 @@ const CreateSet = () => {
         </div>
 
         {cards.map((card, index) => (
-          <div key={index} className='card-container'>
-            <div className='card-frontback-container'>
-              <FloatingLabel label='Front' className='card-container-front'>
+          <div key={index} className="card-container">
+            <div className="card-frontback-container">
+              <FloatingLabel label="Front" className="card-container-front">
                 <Form.Control
-                  type='text'
-                  placeholder='Front'
+                  type="text"
+                  placeholder="Front"
                   value={card.front}
-                  onChange={e => {
+                  onChange={(e) => {
                     const updatedCards = [...cards];
                     updatedCards[index].front = e.target.value;
-                    setCards(updatedCards)
+                    setCards(updatedCards);
                   }}
                 />
               </FloatingLabel>
-              <FloatingLabel label='Back' className='card-container-back'>
+              <FloatingLabel label="Back" className="card-container-back">
                 <Form.Control
-                  type='text'
-                  placeholder='Back'
+                  type="text"
+                  placeholder="Back"
                   value={card.back}
-                  onChange={e => {
+                  onChange={(e) => {
                     const updatedCards = [...cards];
                     updatedCards[index].back = e.target.value;
-                    setCards(updatedCards)
+                    setCards(updatedCards);
                   }}
                 />
-                <FontAwesomeIcon icon={faTrash} onClick={() => handleDelete(index)} />
+                <FontAwesomeIcon
+                  icon={faTrash}
+                  onClick={() => handleDelete(index)}
+                />
               </FloatingLabel>
             </div>
           </div>
         ))}
-        <div className='footer-button-container'>
+        <div className="footer-button-container">
           <Button onClick={() => addCard()}>Add Card</Button>
         </div>
       </Form>
     </div>
-  )
-}
+  );
+};
 
 export default CreateSet;
