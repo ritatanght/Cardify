da0f3c140441c10cc76bef4b87df9b027ff2eceb
ritatanght <doggymint@gmail.com>
2023-11-21 01:00:12 -0500

    Fix eslint warnings

--- server/routes/sets.js
+++ server/routes/sets.js
@@ -2,52 +2,64 @@ const router = require("express").Router();
 const sets = require("../db/queries/sets");
 const cards = require("../db/queries/cards");
 
-router.post('/create', (req, res) => {
-  sets.postSetData(req.body)
-    .then(result => {
-      res.status(201).json({ success: true, message: "Set created successfully", data: result });
+router.post("/create", (req, res) => {
+  sets
+    .postSetData(req.body)
+    .then((result) => {
+      res
+        .status(201)
+        .json({
+          success: true,
+          message: "Set created successfully",
+          data: result,
+        });
     })
-    .catch(err => {
-      res.status(500)
-      console.error(err)
-    })
+    .catch((err) => {
+      res.status(500);
+      console.error(err);
+    });
 });
 
-router.put('/edit/:id', (req, res) => {
-  sets.updateSetData(req.body)
-    .then(response => {
-      res.status(200).json({ message: "Set update successfully", data: response })
+router.put("/edit/:id", (req, res) => {
+  sets
+    .updateSetData(req.body)
+    .then((response) => {
+      res
+        .status(200)
+        .json({ message: "Set update successfully", data: response });
     })
-    .catch(err => {
-      res.status(500)
-      console.error(err)
-    })
-})
+    .catch((err) => {
+      res.status(500);
+      console.error(err);
+    });
+});
 
 router.delete("/delete/:id", (req, res) => {
-  const setId = req.params.id
-  sets.setSetToDeleted(setId)
+  const setId = req.params.id;
+  sets
+    .setSetToDeleted(setId)
     .then((response) => {
-      res.status(200).json({message: "Set deleted", data: response})
+      res.status(200).json({ message: "Set deleted", data: response });
     })
-    .catch(err => {
-      res.status(500)
-      console.error(err)
-    })
-})
+    .catch((err) => {
+      res.status(500);
+      console.error(err);
+    });
+});
 
 router.get("/user/:id", (req, res) => {
   const { id } = req.params;
 
-  sets.getSetsByUserId(id)
-    .then(data => {
-      res.status(200).json(data)
+  sets
+    .getSetsByUserId(id)
+    .then((data) => {
+      res.status(200).json(data);
     })
-    .catch(err => {
-      res.status(500)
-      console.error(err)
-    })
-})
+    .catch((err) => {
+      res.status(500);
+      console.error(err);
+    });
+});
 
 router.get("/:id", (req, res) => {
   const { id } = req.params;
