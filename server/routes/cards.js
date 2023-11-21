73f0b5205ead52b142cbe4f3da9fe5ae62f540ce
ritatanght <doggymint@gmail.com>
2023-11-21 15:57:09 -0500

    Add checking before editing of cards

--- server/routes/cards.js
+++ server/routes/cards.js
@@ -54,12 +54,15 @@ router.put("/update/:id", (req, res) => {
       .json({ success: false, message: "Front and back text cannot be empty" });
   }
 
-  // make sure the user who edits the card is the owner
   const userId = req.session.userId;
+  // make sure the user is logged-in
+  if (!userId) return res.status(401).json({ message: "Please log in first." });
+  
+  // make sure the user who edits the card is the owner
   cards.getCardOwnerByCardId(id).then((data) => {
     if (data.user_id !== userId)
       return res
-        .json(401)
+        .json(403)
         .json({ message: "You can only edit your own card." });
 
     // Update the card
