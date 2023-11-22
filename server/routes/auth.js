6ea514592840d1bf740d1786e37e6ae7e94a5c92
ritatanght <doggymint@gmail.com>
2023-11-21 23:54:09 -0500

    Add more validation for the login route in backend

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -4,8 +4,12 @@ const bcrypt = require("bcrypt");
 
 router.post("/login", (req, res) => {
   const { email, password } = req.body;
+  if (!email || !password)
+    return res.status(400).json({ message: "Login details are incorrect." });
+
   users.getUserByEmail(email).then((user) => {
-    if (!user) return res.status(404).json({ message: "User not found" });
+    if (!user)
+      return res.status(400).json({ message: "Login details are incorrect." });
 
     bcrypt.compare(password, user.hashed_password, (err, result) => {
       // return the userObject when the password is correct
@@ -22,10 +26,10 @@ router.post("/login", (req, res) => {
       }
 
       if (err) {
-        return res.json({ message: err });
+        return res.status(500).json({ message: err });
       }
 
-      return res.json({ message: "Login details are incorrect." });
+      return res.status(400).json({ message: "Login details are incorrect." });
     });
   });
 });
