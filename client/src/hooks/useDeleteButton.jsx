4276a4ce8ee416cbc948026697e3c1e46127577f
ritatanght <doggymint@gmail.com>
2023-11-22 23:10:43 -0500

    Add toastify for message

--- client/src/hooks/useDeleteButton.jsx
+++ client/src/hooks/useDeleteButton.jsx
@@ -1,21 +1,34 @@
 import { useState } from "react";
+import { toast } from "react-toastify";
+import { useUser } from "../context/UserProvider";
 import axios from "axios";
 
 const useDeleteButton = (initialSets = []) => {
-  const [sets, setSets] = useState(initialSets)
+  const { clearUserInfo } = useUser();
+  const [sets, setSets] = useState(initialSets);
 
   const deleteSet = (setId) => {
-    axios.delete(`/api/sets/delete/${setId}`)
-      .then(() => {
-        const updatedSets = sets.filter(set => set.id !== setId);
-        setSets(updatedSets)
+    axios
+      .delete(`/api/sets/delete/${setId}`)
+      .then((res) => {
+        if (res.status === 200) {
+          const updatedSets = sets.filter((set) => set.id !== setId);
+          setSets(updatedSets);
+          toast.success(res.data.message);
+        }
       })
-      .catch(err => {
-        console.error('Error deleting the set: ', err)
-      })
-  }
+      .catch((err) => {
+        if (err.response.status === 401) {
+          toast.info(err.response.data.message);
+          clearUserInfo();
+          //return navigate("/login");
+        } else {
+          console.error("Error deleting the set: ", err);
+        }
+      });
+  };
 
-  return { sets, deleteSet }
-}
+  return { sets, deleteSet };
+};
 
 export default useDeleteButton;
