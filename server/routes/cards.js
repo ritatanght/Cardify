05fc8f3b637e4204954a22371fe155f2d81bbb60
ritatanght <doggymint@gmail.com>
2023-11-24 23:35:21 -0500

    Remove unused endpoints

--- server/routes/cards.js
+++ server/routes/cards.js
@@ -1,48 +1,6 @@
 const router = require("express").Router();
 const cards = require("../db/queries/cards");
 
-// router.post("/create", (req, res) => {
-//   cards
-//     .postCardsData(req.body)
-//     .then((result) => {
-//       res.status(201).json({
-//         success: true,
-//         message: "Cards created successfully",
-//         data: result,
-//       });
-//     })
-//     .catch((err) => {
-//       res.status(500);
-//       console.error(err);
-//     });
-// });
-
-router.put("/edit/:id", (req, res) => {
-  cards
-    .updateCardsData(req.body)
-    .then((result) => {
-      res.status(200).json({ message: "Cards updated", data: result });
-    })
-    .catch((err) => {
-      res.status(500);
-      console.error(err);
-    });
-});
-
-router.get("/:id", (req, res) => {
-  const { id } = req.params;
-  console.log(id);
-  cards
-    .getCardsBySetId(id)
-    .then((data) => {
-      res.json(data);
-    })
-    .catch((err) => {
-      res.status(500);
-      console.error(err);
-    });
-});
-
 router.put("/update/:id", (req, res) => {
   const { id } = req.params;
   const { front, back } = req.body;
