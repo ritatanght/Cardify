d4e859213f75291ed9125b28618d756f331387af
ritatanght <doggymint@gmail.com>
2023-11-25 16:11:55 -0500

    Remove index.css

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import UserProvider from "./context/UserProvider";
 import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./index.css";
-import UserProvider from "./context/UserProvider";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
