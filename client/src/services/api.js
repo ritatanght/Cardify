2bff37d9e05cc0888bed6c745fd10596bbee4a5d
ritatanght <doggymint@gmail.com>
2023-11-29 15:16:58 -0500

    Move axios post for login to services/api

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -3,3 +3,7 @@ import axios from "axios";
 export const getAllCategories = () => {
   return axios.get("/api/categories").then((res) => res.data);
 };
+
+export const logInUser = (loginInfo) => {
+  return axios.post("/api/auth/login", loginInfo).then((res) => res.data);
+};
