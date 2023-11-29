a983663fdd9d406eb7d8ca045de977b9052197c0
ritatanght <doggymint@gmail.com>
2023-11-29 18:04:30 -0500

    Reoganize order of requests

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,17 +1,41 @@
 import axios from "axios";
 
+/* --- Categories --- */
 export const getAllCategories = () => {
   return axios.get("/api/categories").then((res) => res.data);
+};
+
+/* --- Users --- */
+export const registerUser = (userInfo) => {
+  return axios.post("/api/users", userInfo);
 };
 
 export const logInUser = (loginInfo) => {
   return axios.post("/api/auth/login", loginInfo).then((res) => res.data);
 };
 
-export const registerUser = (userInfo) => {
-  return axios.post("/api/users", userInfo);
+export const logOutUser = () => {
+  return axios.post(`/api/auth/logout`);
 };
 
+export const getUserSets = () => {
+  return axios.get("/api/sets/user").then((res) => res.data);
+};
+
+export const getUserFavorites = () => {
+  return axios.get("/api/favorites").then((res) => res.data);
+};
+
+/* --- Favorites --- */
+export const likeSet = (setId) => {
+  return axios.post(`/api/favorites/${setId}`);
+};
+
+export const unlikeSet = (setId) => {
+  return axios.delete(`/api/favorites/${setId}`);
+};
+
+/* --- Sets --- */
 export const createSet = (setData) => {
   return axios.post("/api/sets/create", setData);
 };
@@ -26,34 +50,15 @@ export const searchSets = (query) => {
     .then((res) => res.data);
 };
 
-export const getUserSets = () => {
-  return axios.get("/api/sets/user").then((res) => res.data);
-};
-
-export const getUserFavorites = () => {
-  return axios.get("/api/favorites").then((res) => res.data);
-};
-
-export const logOutUser = () => {
-  return axios.post(`/api/auth/logout`);
+export const editSet = (setId, updatedSetInfo) => {
+  return axios.put(`/api/sets/edit/${setId}`, updatedSetInfo);
 };
 
 export const deleteSetById = (setId) => {
   return axios.delete(`/api/sets/delete/${setId}`);
 };
 
-export const likeSet = (setId) => {
-  return axios.post(`/api/favorites/${setId}`);
-};
-
-export const unlikeSet = (setId) => {
-  return axios.delete(`/api/favorites/${setId}`);
-};
-
-export const editSet = (setId, updatedSetInfo) => {
-  return axios.put(`/api/sets/edit/${setId}`, updatedSetInfo);
-};
-
+/* --- Card --- */
 export const editCardById = (cardId, card) => {
   return axios.put(`/api/cards/update/${cardId}`, card);
 };
