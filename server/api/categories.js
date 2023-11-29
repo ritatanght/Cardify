0be2986a2d00ece2bc2ef510fbc296c1d7873a4f
ritatanght <doggymint@gmail.com>
2023-11-29 17:55:31 -0500

    Add error message when unable to fetch categories

--- server/api/categories.js
+++ server/api/categories.js
@@ -3,7 +3,12 @@ const categories = require("../db/queries/categories");
 const sets = require("../db/queries/sets");
 
 router.get("/", (req, res) => {
-  categories.getAllCategories().then((data) => res.json(data));
+  categories
+    .getAllCategories()
+    .then((data) => res.json(data))
+    .catch(() =>
+      res.status(500).json({ message: "Error fetching categories" })
+    );
 });
 
 router.get("/:id", (req, res) => {
@@ -11,13 +16,11 @@ router.get("/:id", (req, res) => {
   const categoryPromise = categories.getCategoryById(id);
   const setsPromise = sets.getSetsByCategoryId(id);
 
-  Promise.all([categoryPromise, setsPromise]).then(
-    ([category, sets]) => {
-      if (!category) return res.status(404).json({message: "Category not found"});
-      res.json({ category: category.name, sets });
-    }
-
-  );
+  Promise.all([categoryPromise, setsPromise]).then(([category, sets]) => {
+    if (!category)
+      return res.status(404).json({ message: "Category not found" });
+    res.json({ category: category.name, sets });
+  });
 });
 
 module.exports = router;
